4fd3117e8fc82bf6c94ce696ae6f0471236e65b8
jungwoogyun <jwg8910@naver.com>
2023-12-15 15:52:20 +0900

    .

--- js/common.js
+++ js/common.js
@@ -246,34 +246,33 @@ nav_menu_img_items.forEach(item =>{
 
 
     redMarker.bindPopup(markerInfo.popupContent);
-  
-	});
 
 
+});
 
 
-  // 클릭한 위치의 정보 업데이트 함수(아직구현 x)
-	function updateInfoPanel(latlng) {
-		// 클릭한 위치의 위도와 경도 업데이트
-		document.getElementById('latitude').textContent = latlng.lat.toFixed(6);
-		document.getElementById('longitude').textContent = latlng.lng.toFixed(6);
+// 클릭한 위치의 정보 업데이트 함수(아직구현 x)
+// function updateInfoPanel(latlng) {
+// 		// 클릭한 위치의 위도와 경도 업데이트
+// 		document.getElementById('latitude').textContent = latlng.lat.toFixed(6);
+// 		document.getElementById('longitude').textContent = latlng.lng.toFixed(6);
 
-		// 클릭한 위치의 주소를 지오코딩을 통해 가져오기(나중에)
-		fetch('https://api.opencagedata.com/geocode/v1/json?q=' + latlng.lat + '+' + latlng.lng + '&key=YOUR_OPENCAGE_API_KEY')
-			.then(response => response.json())
-			.then(data => {
-				const address = data.results[0].formatted;
-				document.getElementById('address').textContent = address;
-        console.log('address',address);
-			})
-			.catch(error => {
-				console.error('Error fetching address:', error);
-			});
-	}
+// 		// 클릭한 위치의 주소를 지오코딩을 통해 가져오기(나중에)
+// 		fetch('https://api.opencagedata.com/geocode/v1/json?q=' + latlng.lat + '+' + latlng.lng + '&key=YOUR_OPENCAGE_API_KEY')
+// 			.then(response => response.json())
+// 			.then(data => {
+// 				const address = data.results[0].formatted;
+// 				document.getElementById('address').textContent = address;
+//         console.log('address',address);
+// 			})
+// 			.catch(error => {
+// 				console.error('Error fetching address:', error);
+// 			});
+// 	}
 
-
-	// 지도 클릭 이벤트 리스너
-	map.on('click', function (e) {
-		// 클릭한 지점의 정보를 사용하여 정보 패널 내용을 업데이트
-		updateInfoPanel(e.latlng);
-	});
+	// // 지도 클릭 이벤트 리스너
+	// map.on('click', function (e) {
+	// 	// 클릭한 지점의 정보를 사용하여 정보 패널 내용을 업데이트
+  //   alert("A");
+	// 	updateInfoPanel(e.latlng);
+	// });
