dc2c07ba8271334c3d77612a0893f5a70b533823
jungwoogyun <jwg8910@naver.com>
2023-12-20 11:45:42 +0900

    .

---
+++ js/mobile.js
@@ -0,0 +1,25 @@
+  // 리사이즈 이벤트 핸들러 함수
+  function handleResize() {
+    var windowWidth = window.innerWidth;
+
+    const modalEls = document.querySelectorAll('.modal-dialog');
+    if (windowWidth <= 500) {
+        modalEls.forEach((modal)=>{
+            modal.classList.remove('modal-xl');
+            modal.classList.remove('modal-fullsize');
+
+        })
+
+      
+    } else {
+      // 브라우저 크기가 600px 이상이면 메시지를 숨김
+      modal.classList.add('modal-xl');
+      modal.classList.add('modal-fullsize');
+    }
+  }
+
+  // 리사이즈 이벤트에 핸들러 함수 연결
+  window.addEventListener('resize', handleResize);
+
+  // 초기에 한 번 실행하여 현재 브라우저 크기에 대한 상태를 확인
+  handleResize();
